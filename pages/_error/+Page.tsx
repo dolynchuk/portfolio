4a5c2efb4941d0e8b168b8f0a1c17a776bb7a452
Maksym Dolynchuk <dolinchuk2000@gmail.com>
2025-03-11 15:31:56 +0100

    fix styles

--- pages/_error/+Page.tsx
+++ pages/_error/+Page.tsx
@@ -4,18 +4,22 @@ import { usePageContext } from "vike-react/usePageContext";
 export default function Page() {
   const { is404 } = usePageContext();
 
-  return <>
+  return (
     <>
-      <h1>{is404 ? 'Page Not Found' : 'Something went wrong'}</h1>
-      <h2>But you can still contact me here:</h2>
+      <>
+        <h1>{is404 ? "Page Not Found" : "Something went wrong"}</h1>
+        <h2>But you can still contact me here:</h2>
+      </>
+      <h3>
+        <a href="https://calendly.com/dolinchuk2000/15min" target="_blank" rel="noopener noreferrer">
+          Schedule call
+        </a>
+      </h3>
+      <h3>
+        <a href="https://www.linkedin.com/in/maksym-d-13283b110/" target="_blank" rel="noopener noreferrer">
+          LinkedIn
+        </a>
+      </h3>
     </>
-    <div className="mainLinks">
-      <a href="https://calendly.com/dolinchuk2000/15min" target="_blank" rel="noopener noreferrer">
-        Schedule call
-      </a>
-      <a href="https://www.linkedin.com/in/maksym-d-13283b110/" target="_blank" rel="noopener noreferrer">
-        LinkedIn
-      </a>
-    </div>
-  </>
+  );
 }
