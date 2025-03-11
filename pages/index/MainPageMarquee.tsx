47f175baed7024c24e44e0dd76aebb6ac1cdd3bc
Maksym Dolynchuk <dolinchuk2000@gmail.com>
2025-03-11 13:57:45 +0100

    fix marquee

--- pages/index/MainPageMarquee.tsx
+++ pages/index/MainPageMarquee.tsx
@@ -1,35 +1,42 @@
-import Marquee from "react-fast-marquee"
+import Marquee from "react-fast-marquee";
+
+const TECH_LIST = [
+    'React',
+    'Typescript',
+    'Node.js',
+    'GO',
+    'Python',
+    'Docker',
+    'Redux',
+    'Storybook',
+    'MapBox',
+    'Google Maps',
+    'Google Analytics',
+    '.NET',
+    'MySQL', 
+    'MongoDB',
+    'GraphQL',
+    'D3',
+    'Golang',
+    'Terraform',
+    'Salesforce',
+    'AWS',
+    'OpenAPI',
+    'Vite',
+    'React Query',
+    'Tanstack Router',
+    'Playwright',
+    'WebSockets',
+];
 
 export function MainPageMarquee() {
-    return null;
-    return (
-        <Marquee className="marquee" delay={0.5}>
-            <li>React</li>
-            <li>Typescript</li>
-            <li>Node.js</li>
-            <li>GO</li>
-            <li>Python</li>
-            <li>Docker</li>
-            <li>Redux</li>
-            <li>Storybook</li>
-            <li>MapBox</li>
-            <li>Google Maps</li>
-            <li>Google Analytics</li>
-            <li>.NET</li>
-            <li>MySQL</li>
-            <li>MongoDB</li>
-            <li>GraphQL</li>
-            <li>D3</li>
-            <li>Golang</li>
-            <li>Terraform</li>
-            <li>Salesforce</li>
-            <li>AWS</li>
-            <li>OpenAPI</li>
-            <li>Vite</li>
-            <li>React Query</li>
-            <li>Tanstack Router</li>
-            <li>Playwright</li>
-            <li>WebSockets</li>
-        </Marquee>);
-
+  const list = TECH_LIST.map((item) => <li key={item}>{item}</li>);
+  if (import.meta.env.SSR) {
+    return <ul className="marquee fakeMarquee">{list}</ul>;
+  }
+  return (
+    <Marquee className="marquee" delay={0.5}>
+      {list}
+    </Marquee>
+  );
 }
