5194cfcba7a1659524de0d13f5afbd8d098510a7
Maksym Dolynchuk <dolinchuk2000@gmail.com>
2025-03-11 15:03:14 +0100

    fix deps and marquee

--- pages/index/MainPageMarquee.tsx
+++ pages/index/MainPageMarquee.tsx
@@ -1,42 +1,49 @@
-import Marquee from "react-fast-marquee";
+import React from 'react';
+import './styles.css';
 
 const TECH_LIST = [
-    'React',
-    'Typescript',
-    'Node.js',
-    'GO',
-    'Python',
-    'Docker',
-    'Redux',
-    'Storybook',
-    'MapBox',
-    'Google Maps',
-    'Google Analytics',
-    '.NET',
-    'MySQL', 
-    'MongoDB',
-    'GraphQL',
-    'D3',
-    'Golang',
-    'Terraform',
-    'Salesforce',
-    'AWS',
-    'OpenAPI',
-    'Vite',
-    'React Query',
-    'Tanstack Router',
-    'Playwright',
-    'WebSockets',
+  'React',
+  'Typescript',
+  'Node.js',
+  'GO',
+  'Python',
+  'Docker',
+  'Redux',
+  'Storybook',
+  'MapBox',
+  'Google Maps',
+  'Google Analytics',
+  '.NET',
+  'MySQL', 
+  'MongoDB',
+  'GraphQL',
+  'D3',
+  'Golang',
+  'Terraform',
+  'Salesforce',
+  'AWS',
+  'OpenAPI',
+  'Vite',
+  'React Query',
+  'Tanstack Router',
+  'Playwright',
+  'WebSockets',
 ];
 
 export function MainPageMarquee() {
-  const list = TECH_LIST.map((item) => <li key={item}>{item}</li>);
-  if (import.meta.env.SSR) {
-    return <ul className="marquee fakeMarquee">{list}</ul>;
-  }
   return (
-    <Marquee className="marquee" delay={0.5}>
-      {list}
-    </Marquee>
+    <div className="marquee">
+      <ul className="marquee-items">
+        {TECH_LIST.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+      {/* Duplicate the list for seamless looping */}
+      <ul className="marquee-items">
+        {TECH_LIST.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
