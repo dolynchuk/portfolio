7cf2eb7e612f3f796de3391f7cb8c466aea7c5c2
Maksym Dolynchuk <dolinchuk2000@gmail.com>
2025-03-11 12:10:05 +0100

    fix links

--- pages/+Head.tsx
+++ pages/+Head.tsx
@@ -1,15 +1,34 @@
 import React from "react";
 
 export default function HeadDefault() {
+  const base = import.meta.env.BASE_URL;
+  
   return (
     <>
       <meta charSet="UTF-8" />
       <link rel="icon" href="/favicon.ico" type="image/x-icon" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="theme-color" content="white" />
-      <link rel="preload" href="/font/KyivTypeSans-VarGX.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
-      <link rel="preload" href="/font/KyivTypeSerif-VarGX.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
-      <link rel="preload" href="/font/KyivTypeTitling-VarGX.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
-    </>
+      <link
+        rel="preload"
+        href={`${base}font/KyivTypeSans-VarGX.ttf`}
+        as="font"
+        type="font/ttf"
+        crossOrigin="anonymous"
+      />
+      <link
+        rel="preload"
+        href={`${base}font/KyivTypeSerif-VarGX.ttf`}
+        as="font"
+        type="font/ttf"
+        crossOrigin="anonymous"
+      />
+      <link
+        rel="preload"
+        href={`${base}font/KyivTypeTitling-VarGX.ttf`}
+        as="font"
+        type="font/ttf"
+        crossOrigin="anonymous"
+      />
   );
 }
