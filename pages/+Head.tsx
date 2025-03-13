e6a4a982bfba594e3f438c807bf160f54dff77a4
Maksym Dolynchuk <dolinchuk2000@gmail.com>
2025-03-13 13:44:48 +0100

    add ga

--- pages/+Head.tsx
+++ pages/+Head.tsx
@@ -1,4 +1,7 @@
+import ReactGA from "react-ga4";
 import React from "react";
+
+ReactGA.initialize("G-V7PDVH0PW3");
 
 export default function HeadDefault() {
   const base = import.meta.env.BASE_URL;
@@ -9,8 +12,8 @@ export default function HeadDefault() {
       <link rel="icon" href={`${base}favicon.ico`} type="image/x-icon" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="theme-color" content="white" />
-      <link rel="preconnect" href="https://prod.spline.design" crossorigin />
-      <link rel="preconnect" href="https://www.gstatic.com" crossorigin />
+      <link rel="preconnect" href="https://prod.spline.design" crossOrigin="anonymous" />
+      <link rel="preconnect" href="https://www.gstatic.com" crossOrigin="anonymous" />
       <link
         rel="preload"
         href={`${base}font/KyivTypeSans-VarGX.ttf`}
