8367a28866a2f292d036ef51c70a0d4441072297
Maksym Dolynchuk <dolinchuk2000@gmail.com>
2025-03-13 23:39:17 +0100

    fix rendering

--- pages/+config.ts
+++ pages/+config.ts
@@ -1,16 +1,5 @@
-import vikeReact from "vike-react/config";
 import type { Config } from "vike/types";
-import Layout from "../layouts/LayoutDefault.js";
-// https://vike.dev/config
 
 export default {
-  // https://vike.dev/Layout
-  Layout,
-
-  // https://vike.dev/head-tags
-  title: "Maksym Dolynchuk Portfolio",
-  description: "Front End Engineer portfolio website",
-
-  extends: vikeReact,
   prerender: true,
 } satisfies Config;
--- pages/index/+Page.tsx
+++ pages/index/+Page.tsx
@@ -1,6 +1,7 @@
 
 
 import './styles.css';
+import '../styles.css';
 import { Hero } from '../../components/Hero/Hero';
 import { Pixelate } from '../../components/Pixelate/Pixelate';
 import { MainPageInfo } from './MainPageInfo';
